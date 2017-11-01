08f1693e9fe208bf98c52fb568ff272497d12bf4
Gregory Dukes <kcils360@gmail.com>
2017-11-01 16:56:46 -0700

    initial build

---
+++ MultiArray/MultiArray/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] BigArray = new int[,]
+            {
+                { 1, 2, 3},
+                { 4, 5, 6},
+                { 7, 8, 9}
+            };
+
+            Console.WriteLine($"Enter number from 0 to {BigArray.Length}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
