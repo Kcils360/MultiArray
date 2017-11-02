01de16d61d676f78f91c2d037c78ad6ec1ee8364
Gregory Dukes <kcils360@gmail.com>
2017-11-02 08:11:05 -0700

    fixed bug, runs correctly with dynamic changes.

--- MultiArray/MultiArray/Program.cs
+++ MultiArray/MultiArray/Program.cs
@@ -6,17 +6,54 @@ namespace MultiArray
     {
         static void Main(string[] args)
         {
-            int[,] BigArray = new int[,]
+            int[,] bigArray = new int[,]
+                         {
+                {8, 9, 7},
+                {4, 7, 8},
+                {4, 7, 8},
+                {4, 7, 8},
+                {4, 1, 5}
+                         };
+            int r = bigArray.Rank;
+            int numDim = bigArray.Length / (r+1);
+
+            Console.WriteLine($"Enter a number between 0 and {r}");
+            int Dimension = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Enter another number between 0 and {numDim}");
+            int position = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pick a number to set as new value");
+            int val = Convert.ToInt32(Console.ReadLine());
+
+            bigArray[position, Dimension] = val;
+
+            int n = 0;
+            while (n < r)
             {
-                { 1, 2, 3},
-                { 4, 5, 6},
-                { 7, 8, 9}
-            };
+                // now littleArray at position 0 throgh 2 = 0
+                bigArray[Dimension, n] = val;
+                // now each littleArray at user's position changed to 0
+                bigArray[position, n] = val;
+                n++;
+            }
 
-            Console.WriteLine($"Enter number from 0 to {BigArray.Length}");
-
-
-            Console.ReadKey();
+            foreach (int d in bigArray)
+            {
+                
+                Console.Write($"{d} ");
+            }
+            //Console.WriteLine();
+            //Console.Write(bigArray[0, 0] + " ");
+            //Console.Write(bigArray[0, 1] + " ");
+            //Console.Write(bigArray[0, 2] + " \n");
+            //Console.Write(bigArray[1, 0] + " ");
+            //Console.Write(bigArray[1, 1] + " ");
+            //Console.Write(bigArray[1, 2] + " \n");
+            //Console.Write(bigArray[2, 0] + " ");
+            //Console.Write(bigArray[2, 1] + " ");
+            //Console.Write(bigArray[2, 2] + " ");
+            Console.Read();
         }
     }
 }
